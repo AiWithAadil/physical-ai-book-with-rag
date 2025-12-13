9dde4ee8feeae8c7cd37d7d36ca2d4661118691e
sm aadil <155771830+AiWithAadil@users.noreply.github.com>
2025-12-13 18:54:10 +0500

    Update docusaurus.config.js

--- HackathonGIAIC/physical-ai/docusaurus.config.js
+++ HackathonGIAIC/physical-ai/docusaurus.config.js
@@ -19,7 +19,7 @@ const config = {
   organizationName: 'your-org', // Usually your GitHub org/user name.
   projectName: 'physical-ai', // Usually your repo name.
 
-  onBrokenLinks: 'throw',
+  onBrokenLinks: 'warn',
   onBrokenMarkdownLinks: 'warn',
 
   // Even if you don't use internalization, you can use this field to set useful
